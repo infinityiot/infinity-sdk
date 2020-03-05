bf6f33125585b06129020f28fb19e62625a3e4a1
tufanov <697668@gmail.com>
2020-03-05 11:31:35 +1000

    for first testing

--- sdk/src/main/java/com/foxifinder/infinity/sdk/Commands.java
+++ sdk/src/main/java/com/foxifinder/infinity/sdk/Commands.java
@@ -9,16 +9,17 @@ public class Commands {
     }
 
     /**
-     * setAirplaneModeEnabled - turn Airplane mode on or off
-     * @param value, true to on, false to off
-     * @param cb, callback function with result of operation: succes or not
+     * Turn Airplane mode on or off
+     * @param value {@code true} to on, {@code false} to off
+     * @param cb {@link InfinitySDK.CommandCallback} object with the result of the operation:
+     *                                              succeed or not
      */
     public void setAirplaneModeEnabled(boolean value, InfinitySDK.CommandCallback cb) {
         sdk.runCommand("AIRPL_MODE", (value ? "1" : "0"), cb, false);
     }
 
-    public void setMobileDataEnabled(boolean value, InfinitySDK.CommandCallback cb) {
-        sdk.runCommand("MOBILE_DATA", (value ? "1" : "0"), cb, false);
-    }
+//    public void setMobileDataEnabled(boolean value, InfinitySDK.CommandCallback cb) {
+//        sdk.runCommand("MOBILE_DATA", (value ? "1" : "0"), cb, false);
+//    }
 
 }
